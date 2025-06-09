c8fca52936fee023bb6e25242892eea2e44ad488
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-06-09 15:45:12 -0300

    adiciona página Eventos e configura rotas no App para navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import GlobalStyle from './styles/GlobalStyle';
 import Login from './pages/Login';
+import Eventos from './pages/Eventos';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-      <Login />
-    </>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/eventos" element={<Eventos />} />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
